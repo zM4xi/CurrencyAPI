d0c4e8792a959ffcb4042dc13ecb8192e125b902
zM4xi <maxi@zm4xi.de>
2018-08-09 19:11:30 +0200

    Updated to 1.3

--- src/CurrencyAPI.java
+++ src/CurrencyAPI.java
@@ -2,6 +2,7 @@ package de.zm4xi.currencyapi;
 
 import de.zm4xi.currencyapi.database.MySQL;
 import de.zm4xi.currencyapi.object.files.DatabaseFile;
+import de.zm4xi.currencyapi.object.manager.PlayerBalanceManager;
 import lombok.Getter;
 
 import java.sql.ResultSet;
@@ -15,28 +16,61 @@ public class CurrencyAPI {
     protected MySQL mySQL;
 
     @Getter
+    protected static CurrencyAPI instance;
+
+    @Getter
+    protected PlayerBalanceManager playerBalanceManager;
+
+    @Getter
     protected DatabaseFile databaseFile;
 
     public CurrencyAPI() {
+        instance = this;
         databaseFile = new DatabaseFile();
         mySQL = new MySQL(databaseFile.getString("host"), databaseFile.getString("port"), databaseFile.getString("database"), databaseFile.getString("username"), databaseFile.getString("password"));
 
+        this.playerBalanceManager = new PlayerBalanceManager();
     }
 
     public void withdrawBalance(UUID uuid, double amount) {
+        this.playerBalanceManager.getPlayerBalance(uuid).withdrawBalance(amount);
+        /*
+        Version 1.0
+        No Caching
+
         try {
             getMySQL().update("UPDATE currency SET balance = balance - ? WHERE uuid = ?", amount, uuid.toString());
         } catch (SQLException e) {
             e.printStackTrace();
         }
+         **/
+    }
+
+    public void setBalance(UUID uuid, double balance) {
+        this.playerBalanceManager.getPlayerBalance(uuid).setBalance(balance);
+        /*
+        Version 1.0
+        No Caching
+
+        try {
+            getMySQL().update("UPDATE currency SET balance = ? WHERE uuid = ?", balance, uuid.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+         **/
     }
 
     public void depositBalance(UUID uuid, double amount) {
+        this.playerBalanceManager.getPlayerBalance(uuid).depositBalance(amount);
+        /*
+        Version 1.0
+        No Caching
         try {
             getMySQL().update("UPDATE currency SET balance = balance + ? WHERE uuid = ?", amount, uuid.toString());
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        **/
     }
 
     public boolean isCovered(UUID uuid, double amount) {
@@ -56,6 +90,11 @@ public class CurrencyAPI {
     }
 
     public double getBalance(UUID uuid) {
+        return this.playerBalanceManager.getPlayerBalance(uuid).getBalance();
+        /*
+        Version 1.0
+        No Caching
+
         ResultSet rs = null;
         try {
             rs = getMySQL().query("SELECT balance FROM currency WHERE uuid = ?", uuid.toString());
@@ -66,6 +105,7 @@ public class CurrencyAPI {
             e.printStackTrace();
         }
         return 0;
+        */
     }
 
     public LinkedHashMap<UUID, Double> getTop(int limit) {
